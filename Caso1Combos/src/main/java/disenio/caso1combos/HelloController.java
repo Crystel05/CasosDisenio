36bc65b34bfcd521ba4a1f0b7a82e890dbf3dce2
Crystel Montero <crysvane05@estudiantec.cr>
2021-09-20 16:57:43 -0500

    Caso1

---
+++ Caso1Combos/src/main/java/disenio/caso1combos/HelloController.java
@@ -0,0 +1,14 @@
+package disenio.caso1combos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
