8eff21a42951b7353eed754497e6a29fd7f45c18
Crystel Montero <crysvane05@estudiantec.cr>
2021-09-20 16:58:12 -0500

    Caso2

---
+++ Caso2Bolitas/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module disenio.caso2bolitas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens disenio.caso2bolitas to javafx.fxml;
+    exports disenio.caso2bolitas;
+}
